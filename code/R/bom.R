b19584345fb2d39eee1ffed7131829bd63bc63fc
Neil Saunders <neilfws@gmail.com>
2015-02-24 15:36:45 +1100

    theme_bw()

--- code/R/bom.R
+++ code/R/bom.R
@@ -6,12 +6,12 @@ library(ggplot2)
 
 setwd("~/Dropbox/projects/bom")
 
-d <- fromJSON("data/bom.json")
+d <- fromJSON(file = "data/bom.json")
 d <- ldply(d$rows, function(x) as.data.frame(x))
 
 pdf(file = "output/bom.pdf", width = 9, height = 7)
 gg <- ggplot(d) + geom_point(aes(as.Date(date), max, color = factor(predicted))) + 
   geom_point(aes(as.Date(date), min, color = factor(predicted))) +
-                  xlab("Date") + ylab("Temperature")
+                  xlab("Date") + ylab("Temperature") + theme_bw()
 print(gg)
 dev.off()
